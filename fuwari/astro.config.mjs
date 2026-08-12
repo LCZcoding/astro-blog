7f9fb3e8d3d02a209af1775e570f691ef7b73f51
2401_89084930 <2481606011@qq.com>
2026-08-13 00:35:59 +0800

    部署调整

--- fuwari/astro.config.mjs
+++ fuwari/astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "http://72.155.89.172",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
