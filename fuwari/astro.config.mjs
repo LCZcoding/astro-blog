9b7f742a1924646d61bbea853527538e6878d825
2401_89084930 <2481606011@qq.com>
2026-08-13 01:29:15 +0800

    添加workflow自动化CICD

--- fuwari/astro.config.mjs
+++ fuwari/astro.config.mjs
@@ -26,7 +26,7 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "http://72.155.89.172",
+	site: "http://72.155.89.172",//72.155.89.17
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
