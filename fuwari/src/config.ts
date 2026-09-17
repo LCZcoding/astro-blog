4492176c1e21754c27136d9825a7bb8265e2d7a5
LCZcoding <2481606011@qq.com>
2026-09-17 16:22:05 +0800

    新增留言板页面和Giscus评论组件，更新导航栏配置以包含留言板链接

--- fuwari/src/config.ts
+++ fuwari/src/config.ts
@@ -45,6 +45,11 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.Archive,
 		LinkPreset.About,
 		{
+			name: "留言板",
+			url: "/guestbook/", 
+			
+		},
+		{
 			name: "GitHub",
 			url: "https://github.com/LCZcoding", // Internal links should not include the base path, as it is automatically added
 			external: true, // Show an external link icon and will open in a new tab
